1b855eb7cff3c95b83748be93b786c2c59541e59
sebasmaturanac <sebasmaturanac@gmail.com>
2023-08-09 13:46:44 -0300

    add view result

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,17 @@
 function calcularInteres(principal, tasaInteres, tiempo) {
-  // Calcula el monto de interés
   const interes = (principal * tasaInteres * tiempo) / 100;
-
-  // Calcula el monto total a pagar
   const total = principal + interes;
-
+  
   return {
     interes: interes,
-    total: total,
+    total: total
   };
 }
 
-function obtenerNumeroInput(mensaje) {
-  while (true) {
-    const valor = parseFloat(prompt(mensaje));
-    if (!isNaN(valor) && valor > 0) {
-      return valor;
-    } else {
-      console.log("Por favor, ingrese un número válido y positivo.");
-    }
-  }
-}
-
-const montoPrincipal = obtenerNumeroInput("Ingrese el monto del préstamo:");
-const tasaAnual = obtenerNumeroInput("Ingrese la tasa de interés anual (%):");
-const plazoMeses = obtenerNumeroInput("Ingrese el plazo en meses:");
+const montoPrincipal = parseFloat(prompt("Ingrese el monto del préstamo:"));
+const tasaAnual = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
+const plazoMeses = parseInt(prompt("Ingrese el plazo en meses:"));
 
 const resultado = calcularInteres(montoPrincipal, tasaAnual, plazoMeses);
 
-console.log(`Monto de interés: $${resultado.interes.toFixed(2)}`);
-console.log(`Monto total a pagar: $${resultado.total.toFixed(2)}`);
+alert(`Monto de interés: $${resultado.interes.toFixed(2)}\nMonto total a pagar: $${resultado.total.toFixed(2)}`);
